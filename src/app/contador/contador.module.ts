1c7c20f3ed23ff7ee3cfe0bc20f6b60c673150f8
Johnny Alberto Brenes Nuñez <jbrenes-contratista@sysde.com>
2021-10-20 20:58:50 -0500

    primercommit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { ContadorComponet } from './contador/contador.component';
+
+@NgModule({
+  declarations: [
+    ContadorComponet
+  ],
+  exports:[
+    ContadorComponet
+  ],
+  imports: [ ]
+})
+export class ContadorModule { }
